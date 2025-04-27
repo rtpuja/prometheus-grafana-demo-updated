cfe1313c34ba5a3e76bb685a4037832e59e23566
poojakumawat <pooja.kumawat75@gmail.com>
2025-04-27 15:24:27 +0530

    Observability

---
+++ src/main/java/com/example/prometheusgrafana/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.prometheusgrafana.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Prometheus Grafana Demo!";
+    }
+}
